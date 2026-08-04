0d4e84830d63ee8e4691938c72e6bcbdac30e94e
Miguel <Miguelp>
2026-08-03 21:56:24 -0300

    Primeira aula desenvolvimento móvel

---
+++ app/src/main/java/com/ifsc/app/MainActivity.java
@@ -0,0 +1,41 @@
+package com.ifsc.app;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Integer i = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main); // constraint, button e text?
+        TextView tv = findViewById(R.id.text);
+        tv.setText("-");
+
+
+
+        Button button = findViewById(R.id.button);
+        button.setText("Clique aqui");
+        button.setOnClickListener( v -> {
+            i++;
+            tv.setText("Ui six sevenn: " + Integer.toString(i));
+
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
