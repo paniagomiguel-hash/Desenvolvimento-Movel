7c5c79f85c59998e6157aa09e1d7612bd8474a4e
Miguel <Miguel@email>
2026-08-10 22:13:20 -0300

    APP contador simples

--- app/src/main/java/com/ifsc/aula2/MainActivity.java
+++ app/src/main/java/com/ifsc/aula2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ifsc.aula2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
+
+    int contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button button = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(v -> {
+            contador++;
+            textView.setText(Integer.toString(contador));
+
         });
     }
 }
