554d513c912d0be765405c9f2e04fe3a78d6ec1b
Miguel Paniago Flamini <paniagomiguel@gmail.com>
2026-08-17 21:58:01 -0300

    Sorteio em intervalo de números definido pelo usuário

--- app/src/main/java/com/ifsc/aula2/MainActivity.java
+++ app/src/main/java/com/ifsc/aula2/MainActivity.java
@@ -1,23 +1,29 @@
 package com.ifsc.aula2;
 
+import static kotlin.random.RandomKt.nextInt;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import java.util.Random;
 import com.ifsc.aula2.R;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    int contador = 0;
 
+
+    @SuppressLint({"MissingInflatedId", "SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,10 +31,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.button);
         TextView textView = findViewById(R.id.textView);
+        EditText edMin, edMax;
+        edMin = findViewById(R.id.edMin);
+        edMax = findViewById(R.id.edMax);
+        textView.setText("");
 
         button.setOnClickListener(v -> {
-            contador++;
-            textView.setText(Integer.toString(contador));
+
+            String smin = edMin.getText().toString();
+            String smax = edMax.getText().toString();
+
+            if(smin.isEmpty()){
+                edMin.setError("Informe um inteiro");
+                return;
+            }
+
+            if(smax.isEmpty()){
+                edMax.setError("Informe um inteiro");
+                return;
+            }
+            int min = Integer.parseInt(smin);
+            int max = Integer.parseInt(smax);
+
+            if(min > max){
+                Toast.makeText(this, "Defina Mínimo < Máximo", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+
+            Random random = new Random();
+            int n = random.nextInt(min, max);
+            textView.setText(Integer.toString(n));
+
+
+
 
         });
     }
