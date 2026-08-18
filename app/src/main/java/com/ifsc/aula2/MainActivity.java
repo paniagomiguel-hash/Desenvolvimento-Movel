7799a0073c7b39929fae2f024e3148ada1277ab8
Miguel Paniago Flamini <paniagomiguel@gmail.com>
2026-08-17 21:17:01 -0300

    Aula 3

--- app/src/main/java/com/ifsc/aula2/MainActivity.java
+++ app/src/main/java/com/ifsc/aula2/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.ifsc.aula2.R;
+
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,12 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
         Button button = findViewById(R.id.button);
         TextView textView = findViewById(R.id.textView);
 
