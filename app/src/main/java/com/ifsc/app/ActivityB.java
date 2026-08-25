f8f813f6e429394864d6a067ec7f381c58dbd65e
Miguel Paniago Flamini <paniagomiguel@gmail.com>
2026-08-24 22:03:31 -0300

    Intenções em Android

---
+++ app/src/main/java/com/ifsc/app/ActivityB.java
@@ -0,0 +1,16 @@
+package com.ifsc.app;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityB extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+    }
+}
--- app/src/main/java/com/ifsc/app/MainActivity.java
+++ app/src/main/java/com/ifsc/app/MainActivity.java
@@ -1,9 +1,11 @@
 package com.ifsc.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,6 +31,21 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
         Log.d("cicloDaVida", "onCreate");
 
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(view -> {
+            Intent i  = new Intent(this, ActivityB.class);
+            startActivity(i);
+        });
+
+        Button b2 = findViewById(R.id.button2);
+        b2.setOnClickListener(view -> {
+            EditText editText = findViewById(R.id.edText);
+            String s = editText.getText().toString();
+
+            Intent i = new Intent(this, MainActivity2.class);
+            i.putExtra("msg", s);
+            startActivity(i);
+        });
     }
 
     @Override
